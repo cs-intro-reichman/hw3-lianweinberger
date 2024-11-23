7501eb50f2d519a531b65df31e572cb4fd579e92
lianlevwe <lianlevwe@gmail.com>
2024-11-23 21:38:00 +0200

    Finish Anagram

--- Anagram.java
+++ Anagram.java
@@ -2,13 +2,13 @@
 public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+		// System.out.println(isAnagram("silent","listen"));  // true
+		// System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
+		// System.out.println(isAnagram("Madam Curie","Radium came")); // true
+		// System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
 
-		// Tests the preProcess function.
-		System.out.println(preProcess("What? No way!!!"));
+		// // Tests the preProcess function.
+		// System.out.println(preProcess("What? No way!!!"));
 		
 		// Tests the randomAnagram function.
 		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
@@ -28,22 +28,51 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String preProStr1 = preProcess(str1);
+		String preProStr2 = preProcess(str2);
+		boolean isAnagram = true;
+		for (int i = 0; i < preProStr2.length() && isAnagram; i++) {
+			char ch = preProStr2.charAt(i);
+			if (preProStr1.indexOf(ch) == -1) isAnagram = false;
+		}
+
+		return isAnagram;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
-	// as is. For example, the string "What? No way!" becomes "whatnoway"
+	// as is. For example, the string "What? No way!" becomes "what no way"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String finishStr = "";
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') finishStr += (char) (str.charAt(i) + 32);
+			else if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') finishStr += (str.charAt(i));
+		}
+		return finishStr;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
 		// Replace the following statement with your code
-		return "";
+		String randomAnagram = "";
+		String preProcressStr = preProcess(str);
+		int randomNumRange = preProcressStr.length();
+		while (randomNumRange > 0) {
+			int randomNum = (int) (randomNumRange * Math.random());
+			randomAnagram += preProcressStr.charAt(randomNum);
+			preProcressStr = RemoveChar(preProcressStr, preProcressStr.charAt(randomNum));
+			randomNumRange--;
+		}
+		return randomAnagram;
 	}
+
+	public static String RemoveChar(String str, char c) {
+		String newStr = "";
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) != c) newStr += str.charAt(i);
+		}
+		return newStr;
+	}
+
 }
